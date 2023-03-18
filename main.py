1326c52a4efd886319ba9dd70c647d6e192de553
Tho Ha <tho.ha@gatech.edu>
2023-03-18 11:24:51 -0500

    Adding doc strings and getting rid of pylint errors

--- main.py
+++ main.py
@@ -1,72 +1,69 @@
-
-import json
+"""main.py Initial driver for the sudoku Puzzle
+"""
 import os
-import math
 
 from sudoku import Sudoku
 
 
-clear = lambda: os.system('clear')
+clear = os.system('clear')
 
 
-def main():
-    board = Sudoku.Puzzle(9)
-    board.fill(1,1,"2", "True")
-    board.fill(1,3,"3", "True")
-    board.fill(1,4,"6", "True")
-    board.fill(1,5,"5", "True")
-    board.fill(1,7,"7", "True")
+board = Sudoku.Puzzle(9)
+board.fill(1,1,2, True)
+board.fill(1,3,3, True)
+board.fill(1,4,6, True)
+board.fill(1,5,5, True)
+board.fill(1,7,7, True)
 
-    board.fill(2,3,"8", "True")
-    board.fill(2,5,"2", "True")
-    board.fill(2,6,"7", "True")
-    board.fill(2,8,"3", "True")
+board.fill(2,3,8, True)
+board.fill(2,5,2, True)
+board.fill(2,6,7, True)
+board.fill(2,8,3, True)
 
-    board.fill(3,2,"5", "True")
-    board.fill(3,3,"7", "True")
-    board.fill(3,9,"4", "True")
+board.fill(3,2,5, True)
+board.fill(3,3,7, True)
+board.fill(3,9,4, True)
 
-    board.fill(4,1,"8", "True")
-    board.fill(4,9,"2", "True")
+board.fill(4,1,8, True)
+board.fill(4,9,2, True)
 
-    board.fill(5,1,"3", "True")
-    board.fill(5,2,"9", "True")
-    board.fill(5,4,"8", "True")
-    board.fill(5,7,"4", "True")
+board.fill(5,1,3, True)
+board.fill(5,2,9, True)
+board.fill(5,4,8, True)
+board.fill(5,7,4, True)
 
-    board.fill(6,1,"4", "True")
-    board.fill(6,3,"6", "True")
-    board.fill(6,5,"1", "True")
+board.fill(6,1,4, True)
+board.fill(6,3,6, True)
+board.fill(6,5,1, True)
 
-    board.fill(7,5,"9", "True")
-    board.fill(7,8,"1", "True")
+board.fill(7,5,9, True)
+board.fill(7,8,1, True)
 
-    board.fill(8,7,"8", "True")
-    board.fill(8,9,"5", "True")
+board.fill(8,7,8, True)
+board.fill(8,9,5, True)
 
-    board.fill(9,2,"3", "True")
-    board.fill(9,3,"2", "True")
-    board.fill(9,4,"7", "True")
-    board.fill(9,5,"8", "True")
-    board.fill(9,7,"6", "True")
+board.fill(9,2,3, True)
+board.fill(9,3,2, True)
+board.fill(9,4,7, True)
+board.fill(9,5,8, True)
+board.fill(9,7,6, True)
 
-    board.validate()
-    board.brute_force_solve()
-    board.console_print()
-    #valid = True 
-    #while board.not_solved():
-    #    if not valid:
-    #        print("Last Entry Was not a valid Entry")
-    #    else:
-    #        print("")
+board.validate()
+board.brute_force_solve()
+board.pretty_print()
+#valid = True
+#while board.not_solved():
+#    if not valid:
+#        print("Last Entry Was not a valid Entry")
+#    else:
+#        print("")
 
-    #    board.console_print()
-    #    x = input("Input Row: ")
-    #    y = input("Input Column: ")
-    #    value = input("Input Value: ")
-    #    board.fill(int(x), int(y), f'{value}')
-    #    valid = board.validate() 
-    #    if not valid:
-    #        board.fill(int(x), int(y), INVALID)
-    #    clear()
-main()
+#    board.console_print()
+#    x = input("Input Row: ")
+#    y = input("Input Column: ")
+#    value = input("Input Value: ")
+#    board.fill(int(x), int(y), f'{value}')
+#    valid = board.validate()
+#    if not valid:
+#        board.fill(int(x), int(y), INVALID)
+#    clear()
--- sudoku/Sudoku.py
+++ sudoku/Sudoku.py
@@ -1,47 +1,80 @@
+"""Sudoku.py
+This is the base file for the Sudoku puzzle.
+"""
+
 import json
 import math
 
 class Puzzle:
+    """
+    This is the base class class for the sudoku puzzle
+    """
 
-    INVALID = "-1"
+    INVALID = -1
 
-    def __init__(self, size):
+    def __init__(self, size : int):
+        """This is to initalize the class,
+        
+        Keyword Arugments:
+            size (int): The size of the board to build, needs to be square rootable
+        """
         self.board = []
         self.size = size + 1
         self.square_root = math.sqrt(size)
 
         for row in range(1, self.size):
             for column in range(1, self.size):
-                self.board.append({"row": row, "column" : column, "value": self.INVALID, "hints": [], "initial": "False"})
+                self.board.append(
+                        {"row": row,
+                         "column" : column, 
+                         "value": self.INVALID, 
+                         "hints": [], 
+                         "initial": False
+                         }
+                        )
 
-    def fill(self, row, column, value, initial="False"):
-        assert 0 < row <= 9, "invalid row value"
-        assert 0 < column <= 9, "invalid column value"
-        assert 0 < int(value) <= 9 or int(value) == -1, "invalid value"
+    def fill(self, row: int, column: int, value: int, initial: bool = False):
+        """This will fill in the cell sepcified with the value, Returns True if successfully set, 
+        otherwise returns false 
+        Keyword Arugments:
+            row (int): The row of the cell to set.
+            column (int): The column of the cell to set.
+            value (int): The value to set for the cell.
+            initial (bool) default: False: When set to true, this means this cell is a starter cell.
+                                    Starter cells cannot be deleted
+        """
+
+        assert 0 < row <= self.size, "invalid row value"
+        assert 0 < column <= self.size, "invalid column value"
+        assert 0 < value <= self.size or value == -1, "invalid value"
         return self._set_cell(row, column, value, initial)
 
     def not_solved(self):
-        for row in range(1, self.size):
-            for column in range(1, self.size):
-                value = self._get_cell(row, column)['value']
-                if value is self.INVALID:
-                    return True
+        """ Returns True if the puzzle has been solved, False Otherwise
+        """
+        if self.find_empty() is None:
+            return True
         return False
 
     def validate(self):
+        """ Returns False if the puzzle is invalid, otherwise returns True 
+        """
         for row in range(1, self.size):
             if self._validate_row(row) is False:
-                return False 
+                return False
+
         for column in range(1, self.size):
             if self._validate_column(column) is False:
-                return False 
-    
+                return False
+
         for block in range(1, self.size):
             if self._validate_block(block) is False:
                 return False
         return True
 
     def find_empty(self):
+        """ Returns False if the puzzle is invalid, otherwise returns True 
+        """
         for cell in self.board:
             if cell['value'] == self.INVALID:
                 return cell['row'], cell['column']
@@ -49,13 +82,12 @@ class Puzzle:
 
 
     def brute_force_solve(self):
+        """ Brute force solve the puzzle
+        """
         find = self.find_empty()
         if not find:
             return True
-        else:
-            row, column = find
-
-        value = self._get_cell(row, column)['value']
+        row, column = find
 
         for i in range(1, self.size):
             self.fill(row, column, i)
@@ -67,46 +99,90 @@ class Puzzle:
         return False
 
     def __str__(self):
+        """ Tells stuff how to convert this class to a string 
+        """
         return json.dumps(self.board, ensure_ascii=False, indent=4)
 
-    def _set_cell(self, x, y, value, initial = "False"):
+    def _set_cell(self, row: int, column: int, value: int, initial: bool = False):
+        """This will fill in the cell sepcified with the value, Returns True if
+        successful, False otherwise.
+        
+        Keyword Arugments:
+            row (int): The row of the cell to set.
+            column (int): The column of the cell to set.
+            value (int): The value to set for the cell.
+            initial (bool) default: False: When set to true, this means this cell is a starter cell.
+                                    Starter cells cannot be deleted
+        """
+        assert 0 < row <= self.size, "invalid row value"
+        assert 0 < column <= self.size, "invalid column value"
+
         for cell in self.board:
-            if cell["row"] == x and cell["column"] == y:
-                if cell['initial'] != "True":
-                    cell['value'] = f'{value}'
+            if cell["row"] == row and cell["column"] == column:
+                if cell['initial'] is not True:
+                    cell['value'] = value
                     cell['initial'] = initial
                     return True
-                else:
-                    return False
+                return False
 
-    def _get_cell(self, x, y):
+        return False
+    def _get_cell(self, row: int, column: int):
+        """ Internal function to get the cell given the row and column.
+            If you can find the Cell then return the cell, otherwise return None
+        
+        Keyword Arugments:
+            row (int): The row of the cell to set.
+            column (int): The column of the cell to set.
+        """
+        assert 0 < row <= self.size, "invalid row value"
+        assert 0 < column <= self.size, "invalid column value"
+
         for cell in self.board:
-            if cell["row"] == x and cell["column"] == y:
+            if cell["row"] == row and cell["column"] == column:
                 return cell
 
-    def _validate_row(self, row):
+        # Should never get here...
+        return self.board[1][1]
+    def _validate_row(self, row: int):
+        """ Internal function to validate the given row in the puzzle. Returns False if invalid
+            othwerise return True
+
+        Keyword Arugments:
+            row (int): The row of the cell verify.
+        """
         answers = []
         for column in range(1, self.size):
             value = self._get_cell(row, column)['value']
             if value is not self.INVALID:
                 if value in answers:
                     return False
-                else:
-                    answers.append(value)
+                answers.append(value)
         return True
 
-    def _validate_column(self, column):
+    def _validate_column(self, column: int):
+        """ Internal function to validate the given column in the puzzle. Returns False if invalid
+            othwerise return True
+
+        Keyword Arugments:
+            column (int): The column of the cell verify.
+        """
+
         answers = []
         for row in range(1, self.size):
             value = self._get_cell(row, column)['value']
             if value is not self.INVALID:
                 if value in answers:
                     return False
-                else:
-                    answers.append(value)
+                answers.append(value)
         return True
 
-    def _validate_block(self, block):
+    def _validate_block(self, block: int):
+        """ Internal function to validate the given block in the puzzle. Returns False if invalid
+            othwerise return True
+
+        Keyword Arugments:
+            block (int): The block of the cell verify.
+        """
         answers = []
         for cell in self.board:
             if self._get_block(cell['row'], cell['column']) is block:
@@ -117,49 +193,54 @@ class Puzzle:
                     answers.append(value)
         return True
 
-    def console_print(self):
-        str = "   "
-        for row in range(1, self.size):
-            str += f'{row}' + " "
+    def pretty_print(self):
+        """ Pretty prints the board in a CLI GUI. 
+        """
 
-        str += "\n  "
-        for i in range(1, self.size*2):
-            str += "-"
-
-        str += "\n"
+        output = "   "
         for row in range(1, self.size):
-            str += row.__str__() + " |"
+            output += f'{row}' + " "
+
+        output += "\n  "
+        output += "-"*self.size*2
+        output += "\n"
+
+        for row in range(1, self.size):
+            output += f'{row}' + " |"
             for column in range(1, self.size):
                 value = self._get_cell(row, column)['value']
                 if value == self.INVALID:
-                    str += "-"
+                    output += "-"
                 else:
-                    str += value
+                    output += f'{value}'
 
                 if column%self.square_root == 0:
-                    str += "|"
+                    output += "|"
                 else:
-                    str += " "
+                    output += " "
                 if column == (self.size -1):
-                    str += " " + f'{row}'
+                    output += " " + f'{row}'
             if row%self.square_root == 0:
-                str += "\n"
-                str += "  "
-                for i in range(1, self.size*2):
-                    str += "-"
-                str += "\n"
-            else:
-                str += "\n"
+                output += "\n"
+                output += "  "
 
-        str += "   "
+                output += "-"*self.size*2
+            output += "\n"
+
+        output += "   "
         for row in range(1, self.size):
-            str += f'{row}' + " "
-        str += "\n"
+            output += f'{row}' + " "
+        output += "\n"
 
-        print(str)                
+        print(output)
 
-    def _get_block(self, x, y):
-        row = (x-1)//self.square_root
-        column = (y-1)//self.square_root
-        block = (row * self.square_root) + column +1
+    def _get_block(self, row: int, column: int):
+        """ Returns the block the row and column should be in.
+        Keyword Arugments:
+            row (int): The row of the cell to set.
+            column (int): The column of the cell to set.
+        """
+        rows = (row-1)//self.square_root
+        columns = (column-1)//self.square_root
+        block = (rows * self.square_root) + columns +1
         return block
